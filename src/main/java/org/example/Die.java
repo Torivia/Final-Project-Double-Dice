cd2e7e86688bd9e2fd44476f48fed727519d786a
Torivia <tlvargasrubio@gmail.com>
2025-11-16 21:54:25 -0700

    Adding classes and analyzing intructions, also added methods within the classes. Organized by showing objectives at the top. Next commit will initialize variables and further explain what methods will do. Will also reference Die class within DoubleDice class.

---
+++ src/main/java/org/example/Die.java
@@ -0,0 +1,23 @@
+//TODO Constructor method:  Initializes the die’s value to 0 and instantiates the Random object.
+//TODO roll() method – uses the Random object to simulate a die roll between 1 and 6. NOTE: import random
+//TODO equals(Die die2) method – returns Boolean true if both die have equal values, false otherwise.NOTE: if-statement for which bool is returned
+//TODO toString() method – returns the String “one”, “two”, “three”, “four”, “five”, or “six” depending on the value of the die. NOTE: nested if-statement to determine the value and then assign it to the correct string for efficiency :D?
+
+//Die Class – Class that simulates a six-sided die.
+
+package org.example;
+
+public class Die {
+    public static int roll() {
+        return 0;
+    }
+
+    public static boolean equals(Die die2) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/org/example/DoubleDice.java
@@ -0,0 +1,13 @@
+//TODO: Contain main logic for playing the game.
+//TODO: Reference your Die Class and corresponding methods for die rolling.
+//TODO: May either have statements all in a main() method or split logic up in separate methods.
+
+//DoubleDice Class – Class that contains game looping and logic.
+
+package org.example;
+
+public class DoubleDice {
+    public static void main(String[] args) {
+
+    }
+}
